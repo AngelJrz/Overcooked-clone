3df8d5143919d94f25f839219ee0552307508d4b
AngelJrz <Angel_is2020@hotmail.com>
2023-07-02 19:35:41 -0600

    Movement refactor

--- Overcooked/Assets/Scripts/Player/Player.cs
+++ Overcooked/Assets/Scripts/Player/Player.cs
@@ -2,77 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
+
     [SerializeField] private float moveSpeed = 9f;
-    [SerializeField] private Animator animator;
-    private bool isWalking = false;
+    [SerializeField] private GameInput gameInput;
+    private Animator animator;
 
-    void Start()
-    {
+    void Start() {
         this.animator = GetComponentInChildren<Animator>();
     }
 
-    private void Update()
-    {
-        PlayerAcction();
-        ManageAnimation();
-    }
-
-    private void PlayerAcction()
-    {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        // Se usa para no aumentar la velocidad exponencialmente en caso de que se pulsen dos teclas al mismo tiempo
-        inputVector = inputVector.normalized;
-
-        // No tiene caso mantener un vector de 3 axis ya que el input es de 2 dimensiones, lo que conviene es traducir los valores 
-        // y mantener los axis separados
+    private void Update() {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
+        //**********************************
+        // Player movement
+        //**********************************
         transform.position += moveDir * moveSpeed * Time.deltaTime;
-
         float rotateSpeed = Time.deltaTime * 7f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed);
-    }
 
-    private void ManageAnimation()
-    {
-        if (Input.GetKey(KeyCode.W) ||
-            Input.GetKey(KeyCode.S) ||
-            Input.GetKey(KeyCode.A) ||
-            Input.GetKey(KeyCode.D)
-            )
-        {
-            isWalking = true;
-        }
-        else
-        {
-            isWalking = false;
-        }
-
+        //**********************************
+        // Set animation
+        //**********************************
+        bool isWalking = moveDir != Vector3.zero;
         animator.SetBool("IsWalking", isWalking);
-
     }
 }
