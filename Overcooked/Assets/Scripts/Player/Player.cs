e99fa0f091bc326d38f3d7a5f72ae058dc66fbda
AngelJrz <Angel_is2020@hotmail.com>
2023-07-04 19:07:49 -0600

    Selected counter visual

--- Overcooked/Assets/Scripts/Player/Player.cs
+++ Overcooked/Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UIElements;
@@ -6,11 +7,28 @@ using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour {
 
+    public static Player Instance { get; private set; }
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs {
+        public ClearCounter selectedCounter;
+    }
+
+
     [SerializeField] private float moveSpeed = 9f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask layerMask;
     private Animator animator;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
+
+    private void Awake() {
+        if (Instance != null) {
+            Debug.Log("There is more than one player");
+        }
+        Instance = this;
+    }
 
     void Start() {
         this.animator = GetComponentInChildren<Animator>();
@@ -18,18 +36,8 @@ public class Player : MonoBehaviour {
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero) {
-            lastInteractDir = moveDir;
-        }
-
-        float interactDistance = 1f;
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, layerMask)) {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
-                clearCounter.Interact();
-            }
+        if (selectedCounter != null) {
+            selectedCounter.Interact();
         }
     }
 
@@ -49,8 +57,19 @@ public class Player : MonoBehaviour {
         float interactDistance = 1f;
         if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, layerMask)) {
             if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
-                //clearCounter.Interact();
+
+                //if (clearCounter != selectedCounter) {
+                //    selectedCounter = clearCounter;
+                //    SetSelectedCounter(selectedCounter);
+                //}
+
+                // If the player returns to the same counter shows the selected prefab
+                SetSelectedCounter(clearCounter);
+            } else {
+                SetSelectedCounter(null);
             }
+        }else {
+            SetSelectedCounter(null);
         }
     }
 
@@ -105,5 +124,11 @@ public class Player : MonoBehaviour {
         //**********************************
         bool isWalking = moveDir != Vector3.zero;
         animator.SetBool("IsWalking", isWalking);
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter) {
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+            selectedCounter = selectedCounter
+        });
     }
 }
--- Overcooked/Assets/Scripts/furniture/ClearCounter.cs
+++ Overcooked/Assets/Scripts/furniture/ClearCounter.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private ClearCounter clearCounter;
+    [SerializeField] private GameObject visualGameObject;
+
+    private void Start() {
+        Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
+    }
+
+    private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e) {
+        if (e.selectedCounter == clearCounter) {
+            Show();
+        }
+        else {
+            Hide();
+        }
+    }
+
     public void Interact() {
         Debug.Log("Interact");
+    }
+
+    private void Show() {
+        visualGameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        visualGameObject.SetActive(false);
     }
 }
