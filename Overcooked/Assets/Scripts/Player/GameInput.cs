c912bf006ed1faec9d3657dfec8ba0d9df9c074a
AngelJrz <Angel_is2020@hotmail.com>
2023-07-25 21:20:40 -0600

    RemapKeys

--- Overcooked/Assets/Scripts/Player/GameInput.cs
+++ Overcooked/Assets/Scripts/Player/GameInput.cs
@@ -3,20 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour {
 
     public static GameInput Instance { get; private set; }
+
+    private const string PLAYER_PREFS_BINDINGS = "IOnputBindings";
 
     private PlayerInputActions playerInputActions;
     public event EventHandler OnInteractAction;
     public event EventHandler OnInteractAlternateAction;
     public event EventHandler OnPauseInteraction;
 
+    public enum Bindings {
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight,
+        Interact,
+        InteractAlternate
+    }
+
 
     private void Awake() {
         Instance = this;
         playerInputActions = new PlayerInputActions();
+        
+        if (PlayerPrefs.HasKey(PLAYER_PREFS_BINDINGS)) {
+            playerInputActions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_PREFS_BINDINGS));
+        }
+
         playerInputActions.Player.Enable();
 
         playerInputActions.Player.Interact.performed += Interact_performed;
@@ -42,5 +59,76 @@ public class GameInput : MonoBehaviour {
         inputVector = inputVector.normalized;
 
         return inputVector;
+    }
+
+    public string GetKeyBinding(Bindings key) {
+        string keyBindingString = "";
+
+        switch (key) {
+            case Bindings.Interact:
+                keyBindingString = playerInputActions.Player.Interact.GetBindingDisplayString();
+                break; 
+            case Bindings.InteractAlternate:
+                keyBindingString = playerInputActions.Player.InteractAlternate.GetBindingDisplayString();
+                break;
+            case Bindings.MoveUp:
+                keyBindingString = playerInputActions.Player.Move.bindings[1].ToDisplayString();
+                break;
+            case Bindings.MoveDown:
+                keyBindingString = playerInputActions.Player.Move.bindings[2].ToDisplayString();
+                break;
+            case Bindings.MoveLeft:
+                keyBindingString = playerInputActions.Player.Move.bindings[3].ToDisplayString();
+                break;
+            case Bindings.MoveRight:
+                keyBindingString = playerInputActions.Player.Move.bindings[4].ToDisplayString();
+                break;
+        }
+
+        return keyBindingString;
+    }
+
+    public void SetNewKeyBinding(Bindings key, Action callbackAction) {
+        playerInputActions.Player.Disable();
+
+        InputAction inputAction = new();
+        int bindingIndex = 0;
+
+        switch (key) {
+            case Bindings.MoveUp:
+                inputAction = playerInputActions.Player.Move;
+                bindingIndex = 1;
+                break;
+            case Bindings.MoveDown:
+                inputAction = playerInputActions.Player.Move;
+                bindingIndex = 2;
+                break;
+            case Bindings.MoveLeft:
+                inputAction = playerInputActions.Player.Move;
+                bindingIndex = 3;
+                break;
+            case Bindings.MoveRight:
+                inputAction = playerInputActions.Player.Move;
+                bindingIndex = 4;
+                break;
+            case Bindings.Interact:
+                inputAction = playerInputActions.Player.Interact;
+                bindingIndex = 0;
+                break;
+            case Bindings.InteractAlternate:
+                inputAction = playerInputActions.Player.InteractAlternate;
+                bindingIndex = 0;
+                break;
+        }
+
+        inputAction.PerformInteractiveRebinding(bindingIndex)
+            .OnComplete(callback => {
+                callback.Dispose();
+                playerInputActions.Player.Enable();
+                callbackAction();
+                PlayerPrefs.SetString(PLAYER_PREFS_BINDINGS, playerInputActions.SaveBindingOverridesAsJson());
+                PlayerPrefs.Save();
+            })
+            .Start();
     }
 }
--- Overcooked/Assets/Scripts/common/OptionsMenuUI.cs
+++ Overcooked/Assets/Scripts/common/OptionsMenuUI.cs
@@ -12,10 +12,27 @@ public class OptionsMenuUI : MonoBehaviour {
     [SerializeField] private Button effectsVolumeButton;
     [SerializeField] private Button exitButton;
 
+    [SerializeField] private Button moveUpButton;
+    [SerializeField] private Button moveDownButton;
+    [SerializeField] private Button moveLeftButton;
+    [SerializeField] private Button moveRightButton;
+    [SerializeField] private Button interactButton;
+    [SerializeField] private Button interactAlternateButton;
+
     [SerializeField] private TextMeshProUGUI musicVolumeText;
     [SerializeField] private TextMeshProUGUI effectsVolumeText;
 
+    [SerializeField] private TextMeshProUGUI moveUpButtonText;
+    [SerializeField] private TextMeshProUGUI moveDownButtonText;
+    [SerializeField] private TextMeshProUGUI moveLeftButtonText;
+    [SerializeField] private TextMeshProUGUI moveRightButtonText;
+    [SerializeField] private TextMeshProUGUI interactButtonText;
+    [SerializeField] private TextMeshProUGUI interactAlternateButtonText;
+
+    [SerializeField] private Transform remapKeybindingImage;
+
     private void Awake() {
+        instance = this;
 
         musicVolumeButton.onClick.AddListener(() => {
             MusicManager.Instance.ChangeMusicVolume();
@@ -31,9 +48,17 @@ public class OptionsMenuUI : MonoBehaviour {
             Hide();
         });
 
-        instance = this;
+        moveUpButton.onClick.AddListener(() => {
+            UpdateKeyBinding(GameInput.Bindings.MoveUp);
+        });
+
+    }
+
+    private void Start() {
         GameManager.Instance.OnUnPause += Instance_OnUnPause;
+        UpdateKeyBindings();
         UpdateVisuals();
+        HideRemapBackground();
         Hide();
     }
 
@@ -46,11 +71,36 @@ public class OptionsMenuUI : MonoBehaviour {
         effectsVolumeText.text = "Effects volume: " + Mathf.Round(SoundManager.instance.GetVolume() * 10f);
     }
 
+    public void UpdateKeyBindings(){
+        moveUpButtonText.text = GameInput.Instance.GetKeyBinding(GameInput.Bindings.MoveUp);
+        moveDownButtonText.text = GameInput.Instance.GetKeyBinding(GameInput.Bindings.MoveDown);
+        moveLeftButtonText.text = GameInput.Instance.GetKeyBinding(GameInput.Bindings.MoveLeft);
+        moveRightButtonText.text = GameInput.Instance.GetKeyBinding(GameInput.Bindings.MoveRight);
+        interactButtonText.text = GameInput.Instance.GetKeyBinding(GameInput.Bindings.Interact);
+        interactAlternateButtonText.text = GameInput.Instance.GetKeyBinding(GameInput.Bindings.InteractAlternate);
+    }
+
     public void Show() {
         gameObject.SetActive(true);
     }
 
     public void Hide() {
         gameObject.SetActive(false);
+    }
+
+    public void ShowRemapBackground() {
+        remapKeybindingImage.gameObject.SetActive(true);
+    }
+
+    public void HideRemapBackground() {
+        remapKeybindingImage.gameObject.SetActive(false);
+    }
+
+    public void UpdateKeyBinding(GameInput.Bindings key) {
+        ShowRemapBackground();
+        GameInput.Instance.SetNewKeyBinding(key, () => {
+            HideRemapBackground();
+            UpdateKeyBindings();
+        });
     }
 }
